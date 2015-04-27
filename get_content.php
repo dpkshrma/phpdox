fa840b703d07e5be0874a32d00fd7ad29563afc4
Deepak Sharma <dpkshrma01@gmail.com>
2015-04-27 11:07:12 +0530

    created PhpDox class

--- get_content.php
+++ get_content.php
@@ -8,119 +8,153 @@
  * ---------------------------------------------
  */
 
-ini_set('display_errors', false);
-error_reporting(0);
-
 require_once dirname(__FILE__)."/phpclicolors.php";
-
-$j=0;
-
-if (PHP_SAPI == "cli") $lb = "\n";
-else $lb = "<br />";
-
-$colors = new Colors();
-
-$file_name = $argv[1];
-$file_name = preg_replace('/_/','-',$file_name);
-$file_name = "function.".$file_name.".html";
-$file_exists = false;
-
-if ( file_exists(dirname(__FILE__).'/functions/'.$file_name) ) {
-	$file_exists = true;
+/**
+ * Class to search and display the documentation
+ * of required function
+ */
+class PhpDox
+{
+    /**
+     * @var string containing the new line character
+     */
+    protected static $lineBreak = "\n";
+    /**
+     * @var string containing the name of the file to
+     * be searched
+     */
+    protected $fileName;
+    /**
+     * @var object to display colored messages on the console
+     */
+    protected $colorStringObj;
+    public function __construct($searchString = null)
+    {
+        /**
+         * Check if search string is passed in the constructor
+         */
+        if ($searchString === null) {
+            throw new Exception("Search string is missing");
+        }
+        /**
+         * Turn off the error reporting
+         */
+        error_reporting(0);
+        /**
+         * Disable XML errors
+         */
+        libxml_use_internal_errors(true);
+        /**
+         * Set the line break character
+         */
+        self::$lineBreak = "\n";
+        /**
+         * Set the file name according to the naming
+         * convention of the HTML function files
+         */
+        $this->fileName = "function.".preg_replace('/_/','-',$searchString).".html";
+        /**
+         * Initialize object of colors class
+         */
+        $this->colorStringObj = new Colors();
+    }
+    /**
+     * Function to check if file exists for a given function
+     *
+     * @return bool true if file of function exists
+     */
+    protected function fileExists()
+    {
+        if (file_exists(dirname(__FILE__)."/functions/".$this->fileName)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * Echo the usage instruction on the console
+     */
+    public static function printUsageIns()
+    {
+        echo "Usage: phpdox <function_name>".self::$lineBreak;
+    }
+    /**
+     * Function to echo the result to the terminal
+     */
+    public function printResult()
+    {
+        if (!$this->fileExists()) {
+            echo $this->colorStringObj->getColoredString("Function not found.", "red").self::$lineBreak;
+        } else {
+            $htmlContent = file_get_contents(dirname(__FILE__)."/functions/".$this->fileName);
+            $dom = new DOMDocument();
+            $dom->loadHTML($htmlContent);
+            $xpath = new DOMXPath($dom);
+            $functionName = $xpath->query('//h1[@class="refname"]');
+            $functionVersionInfo = $xpath->query('//p[@class="verinfo"]');
+            $functionTitle = $xpath->query('//span[@class="dc-title"]');
+            $functionDesc = $xpath->query('//div[@class="methodsynopsis dc-description"]');
+            $functionDescAlias = $xpath->query('//p[@class="simpara"]');
+            $functionRdfsComment = $xpath->query('//p[@class="para rdfs-comment"]');
+            $functionParams = $xpath->query('//dt');
+            $functionParamDesc = $xpath->query('//dd');
+            $functionReturnVal = $xpath->query('//div[@class="refsect1 returnvalues"]/p[@class="para"]');
+            echo $this->colorStringObj->getColoredString(
+                    trim(strip_tags($functionName->item(0)->nodeValue)),
+                    "light_red"
+                ).self::$lineBreak;
+            echo $this->colorStringObj->getColoredString(
+                    " -- ".trim(strip_tags($functionTitle->item(0)->nodeValue)),
+                    "white"
+                ).self::$lineBreak;
+            echo self::$lineBreak.$this->colorStringObj->getColoredString("Version info", "light_blue").self::$lineBreak;
+            echo $this->colorStringObj->getColoredString(
+                    trim(strip_tags($functionVersionInfo->item(0)->nodeValue)),
+                    "light_gray"
+                ).self::$lineBreak;
+            echo self::$lineBreak.$this->colorStringObj->getColoredString("Description", "light_blue").self::$lineBreak;
+            if ($functionDesc->item(0) != null) {
+                echo $this->colorStringObj->getColoredString(
+                        trim(strip_tags(preg_replace('/\s+/', ' ', $functionDesc->item(0)->nodeValue))),
+                        "light_gray"
+                    ).self::$lineBreak;
+            }
+            if ($functionDescAlias->item(0)!=null) {
+                echo $this->colorStringObj->getColoredString(
+                        trim(strip_tags(preg_replace('/\s+/', ' ', $functionDescAlias->item(0)->nodeValue))),
+                        "light_gray").self::$lineBreak;
+            }
+            if (is_object($functionRdfsComment)) {
+                echo $this->colorStringObj->getColoredString(
+                        trim(strip_tags($functionRdfsComment->item(0)->nodeValue)),
+                        "light_gray").self::$lineBreak;
+            }
+            echo self::$lineBreak.$this->colorStringObj->getColoredString("Parameters", "light_blue").self::$lineBreak;
+            $par = array();
+            $par_desc = array();
+            foreach ($functionParams as $parameter) {
+                $param = $parameter->nodeValue;
+                $par[] = preg_replace('/\s+/', ' ',$param);
+            }
+            foreach ($functionParamDesc as $parameter_desc) {
+                $param_desc = $parameter_desc->nodeValue;
+                $par_desc[] = preg_replace('/\s+/', ' ',$param_desc);
+            }
+            for ($i=0; $i <count($par) ; $i++) {
+                echo $this->colorStringObj->getColoredString(strip_tags($par[$i]),"green").self::$lineBreak;
+                echo $this->colorStringObj->getColoredString(strip_tags($par_desc[$i]),"light_gray").self::$lineBreak.self::$lineBreak;
+            }
+            echo $this->colorStringObj->getColoredString("Return Value", "light_blue").self::$lineBreak;
+            echo $this->colorStringObj->getColoredString(
+                    trim(strip_tags($functionReturnVal->item(0)->nodeValue)),
+                    "light_gray"
+                ).self::$lineBreak;
+        }
+    }
 }
-
-echo $lb;
-
-if ($argv[1] == null || $argv[1] == '') {
-	echo "Usage : phpdox <function_name>".$lb;
-	echo $lb;
-	die();
+if (!isset($argv[1])) {
+    PhpDox::printUsageIns();
+} else {
+    $docObject = new PhpDox($argv[1]);
+    $docObject->printResult();
 }
-
-if (!$file_exists && $argv[1] != '' && $argv[1] != null ) {
-	echo $colors->getColoredString("Requeted function does not exist in the database","light_red").$lb;
-	echo $lb;
-	die();
-
-	// TODO : Suggest similar functions
-/*	foreach (new DirectoryIterator(dirname(__FILE__).'/functions/') as $file) {
-		if($file->isDot()) break;
-		$file_name = $file->getFilename()."$lb";
-	}*/
-}
-else{
-	$file = $file_name;
-
-
-	$html = file_get_contents(dirname(__FILE__)."/functions/".$file);
-
-	$dom = new DOMDocument();
-    libxml_use_internal_errors(true);
-	$dom->loadHTML($html);
-	$xpath = new DOMXPath($dom);
-
-	$classname="refname";
-	$fn_name = $xpath->query('//h1[@class="refname"]');
-	$fn_verinfo = $xpath->query('//p[@class="verinfo"]');
-	$fn_dctitle = $xpath->query('//span[@class="dc-title"]');
-
-	//function syntax
-	$fn_desc = $xpath->query('//div[@class="methodsynopsis dc-description"]');
-	$fn_desc_alias = $xpath->query('//p[@class="simpara"]');
-	$fn_rdfscomment = $xpath->query('//p[@class="para rdfs-comment"]');
-	$fn_parameters = $xpath->query('//dt');
-	$fn_parameters_desc = $xpath->query('//dd');	
-	$fn_returnvals = $xpath->query('//div[@class="refsect1 returnvalues"]/p[@class="para"]');
-
-	// $fn_example_titles = $xpath->query('//div[@class="example"]/p/strong');
-	// $fn_example_desc = $xpath->query('//div[@class="example-contents"]');
-
-	echo $colors->getColoredString(trim(strip_tags($fn_name->item(0)->nodeValue)),"light_red");
-	echo $lb.$colors->getColoredString(" -- ".trim(strip_tags($fn_dctitle->item(0)->nodeValue)),"white")."$lb";
-	echo $lb.$colors->getColoredString("Version info","light_blue").$lb;
-	echo $colors->getColoredString(trim(strip_tags($fn_verinfo->item(0)->nodeValue)),"light_gray").$lb;
-	echo $lb.$colors->getColoredString("Description","light_blue").$lb;
-	if ($fn_desc->item(0) != null) {
-		echo $colors->getColoredString(trim(strip_tags(preg_replace('/\s+/', ' ',$fn_desc->item(0)->nodeValue))),"light_gray")."$lb";
-	}
-	if ($fn_desc_alias->item(0)!=null) {
-		echo $colors->getColoredString(trim(strip_tags(preg_replace('/\s+/', ' ',$fn_desc_alias->item(0)->nodeValue))),"light_gray")."$lb";
-	}
-
-	if (is_object($fn_rdfscomment)) {
-		echo $colors->getColoredString(trim(strip_tags($fn_rdfscomment->item(0)->nodeValue)),"light_gray")."$lb";
-	}
-
-	echo $lb.$colors->getColoredString("Parameters","light_blue").$lb;
-	$par = array();
-	$par_desc = array();
-	foreach ($fn_parameters as $parameter) {
-		$param = $parameter->nodeValue;
-		$par[] = preg_replace('/\s+/', ' ',$param);
-	}
-	foreach ($fn_parameters_desc as $parameter_desc) {
-		$param_desc = $parameter_desc->nodeValue;
-		$par_desc[] = preg_replace('/\s+/', ' ',$param_desc);
-	}
-	for ($i=0; $i <count($par) ; $i++) { 
-		echo $colors->getColoredString(strip_tags($par[$i]),"green").$lb;
-		echo $colors->getColoredString(strip_tags($par_desc[$i]),"light_gray").$lb.$lb;
-	}
-	echo $colors->getColoredString("Return Value","light_blue").$lb;
-	echo $colors->getColoredString(trim(strip_tags($fn_returnvals->item(0)->nodeValue)),"light_gray")."$lb";
-	// TODO : Option to view Examples
-/*	echo "8 Examples : $lb";
-	$eg_titles = array();
-	$eg_desc = array();
-	foreach ($fn_example_titles as $title) {
-		$eg_titles[] = $title->nodeValue;
-	}
-	foreach ($fn_example_desc as $desc) {
-		$eg_desc[] = $desc->nodeValue;
-	}
-	for ($i=0; $i <count($eg_titles) ; $i++) { 
-		echo "<b>".trim(strip_tags($eg_titles[$i])).$lb."</b>";	
-		echo $eg_desc[$i].$lb;	
-	}*/
-}
-echo "$lb";
